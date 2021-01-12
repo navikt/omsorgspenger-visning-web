0cb0bc11d1ca1d6f7b9174ac9f9dc4dd027d0519
eras-dev <76997730+eras-dev@users.noreply.github.com>
2021-01-12 08:44:15 +0100

    Update src/app/containers/Kvote/KvoteContainer.tsx
    Co-authored-by: TomasEng <tomas.engebretsen@nav.no>

--- src/app/containers/Kvote/KvoteContainer.tsx
+++ src/app/containers/Kvote/KvoteContainer.tsx
@@ -14,38 +14,38 @@ interface Props {
 };
 
 export const KvoteContainer: React.FunctionComponent<Props> = ({saksnummer}) => {
-    const {t} = useTranslation();
-    const {data, error, loading} = useGet(`${apiRoutes().Kvote}?status=Aktiv&saksnummer=${saksnummer}`);
-    const errorStatus = error?.response?.status;
-    const år = new Date().getFullYear().toString();
+  const {t} = useTranslation();
+  const {data, error, loading} = useGet(`${apiRoutes().Kvote}?status=Aktiv&saksnummer=${saksnummer}`);
+  const errorStatus = error?.response?.status;
+  const år = new Date().getFullYear().toString();
 
-    return (
-        <>
-            <RammemeldingOverskrift>
-                <CalendarIcon />
-                {t('kvote.overskrift', {år})}
-            </RammemeldingOverskrift>
+  return (
+    <>
+      <RammemeldingOverskrift>
+        <CalendarIcon />
+        {t('kvote.overskrift', {år})}
+      </RammemeldingOverskrift>
 
-            {loading && <LoadingIndicator/>}
+      {loading && <LoadingIndicator/>}
 
-            {error && (<p>
-                {
-                    errorStatus === 404 ? (
-                        t('sak.feil.404', {saksnummer})
-                    ) : (
-                        t('sak.feil.ukjent')
-                    )
-                }
-            </p>)}
+      {error && (<p>
+        {
+          errorStatus === 404 ? (
+            t('sak.feil.404', {saksnummer})
+          ) : (
+            t('sak.feil.ukjent')
+          )
+        }
+      </p>)}
 
-            {data !== null && data.dager !== null &&
-              <PanelMedDagerOgBeskrivelse
-                farge={navColors.navGronn}
-                beskrivelseAvDager={`${t('kvote.antallDagerBeskrivelse', {år})}`}
-                antallDager={data.dager}
-              />}
-            </>
-    );
+      {data !== null && data.dager !== null &&
+        <PanelMedDagerOgBeskrivelse
+          farge={navColors.navGronn}
+          beskrivelseAvDager={`${t('kvote.antallDagerBeskrivelse', {år})}`}
+          antallDager={data.dager}
+        />}
+    </>
+  );
 }
 
 export default KvoteContainer;
