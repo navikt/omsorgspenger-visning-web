9f2c7581346b507f0159b0100e0422a5b12eca11
Rasmusson <emma.rasmusson@nav.no>
2021-02-01 13:27:00 +0100

    Skapade PersonKort som viser saksnummer og födselsnummer

---
+++ src/app/containers/PersonKort/PersonKort.tsx
@@ -0,0 +1,48 @@
+import {apiRoutes} from "../../../utils/http/apiConfig";
+import {LoadingIndicator} from "../../components/LoadingIndicator";
+import {PersonCard} from "@navikt/nap-person-card";
+import React from 'react';
+import styled from "styled-components";
+import useGet from "../../../utils/http/useGet";
+import {useTranslation} from "react-i18next";
+
+interface Props {
+  saksnummer: string;
+}
+
+const PersonKort: React.FunctionComponent<Props> = ({saksnummer}) => {
+  const {data, error, loading} = useGet(
+    `${apiRoutes().Overføringer}?status=Aktiv&saksnummer=${saksnummer}`,
+  );
+  const errorStatus = error?.response?.status;
+  const {t} = useTranslation();
+
+  return (
+    <>
+      {loading && <LoadingIndicator/>}
+      {data !== null &&
+      <PersonCardStyle>
+        <PersonCard name={`${t('personkort.saksnummer')}: ${data.saksnummer}`}
+                    fodselsnummer={`${t('personkort.fodselsnummer')}: ${data.identitetsnummer}`} gender={'unknown'}/>
+      </PersonCardStyle>}
+      {error && <p>
+        {errorStatus === 404
+          ? t('sak.feil.404', {saksnummer})
+          : t('sak.feil.ukjent')}
+      </p>}
+    </>
+  )
+}
+
+const PersonCardStyle = styled.div`
+  margin-bottom: 0;
+  .clipboard__animationContainer{
+    display: none;
+  }
+
+  .person-card__name-gender-container{
+    padding-left: 0;
+  }
+`;
+
+export default PersonKort;
