aecbf15db5b4c7919fab4f11e6872e970895a454
eras-dev <76997730+eras-dev@users.noreply.github.com>
2021-11-15 11:02:14 +0100

    Pkg upgrades 1 (#394)
    * minor upgrades
    
    * major package upgrades
    
    * stylelint updates
    
    * major updates

--- src/app/state/auth/__tests__/authActions.test.ts
+++ src/app/state/auth/__tests__/authActions.test.ts
@@ -3,7 +3,7 @@ import { AuthAction } from '../authActions';
 describe('authActions', () => {
   test('Auth actions skal begynne med "Auth_"', () => {
     Object.values(AuthAction).forEach(authName => {
-      expect(authName).toStartWith('Auth_');
+      expect(authName.substr(0,5)).toBe('Auth_');
     });
   });
 });
