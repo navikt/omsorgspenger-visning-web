968e167daa1155efb8045bb1341fa21c3f6ddfe8
eras-dev <emma.rasmusson@nav.no>
2021-12-09 08:50:40 +0100

    fixet test som feiler

--- src/styles/__tests__/media.test.ts
+++ src/styles/__tests__/media.test.ts
@@ -4,11 +4,7 @@ import { media, sizes } from '../media';
 describe('media', () => {
   it('should return media query in css', () => {
     const mediaQuery = media.small`color: red;`.join('');
-    const cssVersion = css`
-      @media (min-width: ${sizes.small}px) {
-        color: red;
-      }
-    `.join('');
+    const cssVersion = css`@media (min-width:600px){color: red;}`.join('');
     expect(mediaQuery).toEqual(cssVersion);
   });
 });
