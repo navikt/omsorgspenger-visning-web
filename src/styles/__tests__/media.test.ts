6898a308ef7f2a2edcebea3bbde6da7280c4ea8a
eras-dev <emma.rasmusson@nav.no>
2021-12-09 10:35:54 +0100

    fjerner whitespace på css versjon medeeee

--- src/styles/__tests__/media.test.ts
+++ src/styles/__tests__/media.test.ts
@@ -4,7 +4,7 @@ import { media, sizes } from '../media';
 describe('media', () => {
   it('should return media query in css', () => {
     const mediaQuery = media.small`color: red;`.join('').replace(/(\r\n|\n|\r|\s)/gm, '');
-    const cssVersion = css`@media (min-width:600px){color: red;}`.join('');
+    const cssVersion = css`@media (min-width:600px){color: red;}`.join('').replace(/(\s)/gm, '');
     expect(mediaQuery.toLowerCase()).toEqual(cssVersion.toLowerCase());
   });
 });
